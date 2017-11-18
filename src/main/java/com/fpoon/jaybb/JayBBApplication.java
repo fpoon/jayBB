cff72e209350db891443b3cfca874e85d9d64844
Mariusz Rebandel <mariusz.rebandel@silksh.pl>
2017-11-18 17:00:29 +0100

    Initial setup

---
+++ src/main/java/com/fpoon/jaybb/JayBBApplication.java
@@ -0,0 +1,11 @@
+package com.fpoon.jaybb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JayBBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JayBBApplication.class);
+    }
+}
